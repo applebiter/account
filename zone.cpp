bf4d8ddd2f3e8f2d94c553eac33d9cf97ac3da31
Richard Lucas <webmaster@applebiter.com>
2022-09-14 13:25:46 -0400

    Finished first pass over entities. I think I added CMakeLists.txt.user to .gitignore.

--- zone.cpp
+++ zone.cpp
@@ -3,5 +3,176 @@
 Zone::Zone(QObject *parent)
     : QObject{parent}
 {
+    this->create();
+}
 
+void Zone::begin()
+{
+    QSqlDatabase::database().transaction();
+}
+
+void Zone::commit()
+{
+    QSqlDatabase::database().commit();
+}
+
+void Zone::rollback()
+{
+    QSqlDatabase::database().rollback();
+}
+
+bool Zone::open()
+{
+    QSqlDatabase db = QSqlDatabase::database();
+    bool isOpen = db.isOpen();
+
+    return isOpen;
+}
+
+void Zone::create()
+{
+    this->countryId = 0;
+    this->id = 0;
+    this->name = "";
+}
+
+bool Zone::load(quint32 ident)
+{
+    QSqlQuery query;
+    QString cmd = "SELECT country_id, id, name FROM account.zones where id = :id;";
+    query.prepare(cmd);
+    query.bindValue(":id", ident);
+
+    bool ok = this->exec(query);
+
+    if (ok)
+    {
+        while (query.next())
+        {
+            QSqlRecord record = query.record();
+            this->countryId = record.value(0).toInt();
+            this->id = record.value(2).toInt();
+            this->name = record.value(3).toString();
+        }
+    }
+
+    return ok;
+}
+
+bool Zone::save()
+{
+    if (this->id > 0)
+    {
+        return this->update();
+    }
+    else
+    {
+        return this->insert();
+    }
+}
+
+void Zone::remove()
+{
+    QSqlQuery query;
+    QString cmd = "DELETE FROM account.zones WHERE id = :id";
+    query.prepare(cmd);
+    query.bindValue(":id", this->id);
+
+    this->exec(query);
+}
+
+quint32 Zone::getCountryId() const
+{
+    return this->countryId;
+}
+
+void Zone::setCountryId(quint32 newCountryId)
+{
+    if (this->countryId == newCountryId)
+        return;
+    this->countryId = newCountryId;
+    emit this->countryIdChanged();
+}
+
+quint32 Zone::getId() const
+{
+    return this->id;
+}
+
+void Zone::setId(quint32 newId)
+{
+    if (this->id == newId)
+        return;
+    this->id = newId;
+    emit this->idChanged();
+}
+
+const QString &Zone::getName() const
+{
+    return this->name;
+}
+
+void Zone::setName(const QString &newName)
+{
+    if (this->name == newName)
+        return;
+    this->name = newName;
+    emit this->nameChanged();
+}
+
+bool Zone::insert()
+{
+    QSqlQuery query;
+    QString cmd = "INSERT INTO account.zones (country_id, name) VALUES (:country_id, :name);";
+    query.prepare(cmd);
+    query.bindValue(":country_id", this->countryId);
+    query.bindValue(":name", this->name);
+
+    bool ok = exec(query);
+
+    if (ok)
+    {
+        this->id = query.lastInsertId().toInt();
+    }
+    else
+    {
+        this->id = 0;
+    }
+
+    return ok;
+}
+
+bool Zone::update()
+{
+    QSqlQuery query;
+    QString cmd = "UPDATE account.zones SET country_id = :country_id, name = :name WHERE id = :id;";
+    query.prepare(cmd);
+    query.bindValue(":country_id", this->countryId);
+    query.bindValue(":name", this->name);
+    query.bindValue(":id", this->id);
+
+    bool ok = exec(query);
+
+    return ok;
+}
+
+bool Zone::exec(QSqlQuery &query)
+{
+    QSqlDatabase db = QSqlDatabase::database();
+
+    if (!db.isOpen())
+    {
+        return false;
+    }
+
+    //qInfo() << "Exec: " << query.executedQuery();
+    bool ok =  query.exec();
+
+    if (!ok)
+    {
+        qInfo() << db.lastError().text();
+        qInfo() << query.lastError().text();
+    }
+
+    return ok;
 }
--- zone.h
+++ zone.h
@@ -2,15 +2,61 @@
 #define ZONE_H
 
 #include <QObject>
+#include <QDebug>
+#include <QtSql>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include <QVariant>
 
 class Zone : public QObject
 {
     Q_OBJECT
+
 public:
+
     explicit Zone(QObject *parent = nullptr);
+    void begin();
+    void commit();
+    void rollback();
+    bool open();
+    void create();
+    bool load(quint32 ident);
+    bool save();
+    void remove();
+
+    quint32 getCountryId() const;
+    void setCountryId(quint32 newCountryId);
+
+    quint32 getId() const;
+    void setId(quint32 newId);
+
+    const QString &getName() const;
+    void setName(const QString &newName);
 
 signals:
 
+    void countryIdChanged();
+    void idChanged();
+    void nameChanged();
+
+public slots:
+
+private:
+
+    quint32 countryId;
+    quint32 id;
+    QString name;
+
+private:
+
+    bool insert();
+    bool update();
+    bool exec(QSqlQuery &query);
+
+    Q_PROPERTY(quint32 countryId READ getCountryId WRITE setCountryId NOTIFY countryIdChanged)
+    Q_PROPERTY(quint32 id READ getId WRITE setId NOTIFY idChanged)
+    Q_PROPERTY(QString name READ getName WRITE setName NOTIFY nameChanged)
 };
 
 #endif // ZONE_H
