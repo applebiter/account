3fd9aa53cef01a3cbe9af326d87f242db08a7acb
Richard Lucas <webmaster@applebiter.com>
2022-09-14 22:22:24 -0400

    Continuing work on table models.

--- preferencestable.h
+++ preferencestable.h
@@ -1,16 +1,38 @@
 #ifndef PREFERENCESTABLE_H
 #define PREFERENCESTABLE_H
 
+#include <QList>
 #include <QObject>
+#include <QVariant>
+#include "preference.h"
 
 class PreferencesTable : public QObject
 {
     Q_OBJECT
-public:
-    explicit PreferencesTable(QObject *parent = nullptr);
 
-signals:
+    public:
 
+        explicit PreferencesTable(QObject *parent = nullptr);
+        Preference* newEmptyPreference(QObject &a);
+        Preference* newPreference(QObject &a, QHash<QString, QVariant> data);
+        QList<Preference*> newPreferences(QObject &a, QHash<QString, QVariant> data, QHash<QString, QVariant> options);
+        Preference* getPreference(QObject &a, quint32 id);
+        Preference* patchPreference(Preference *preference, QHash<QString, QVariant> data);
+        Preference* save(Preference *preference);
+        QList<Preference*> saveMany(QList<Preference*> preferences, QHash<QString, QVariant> options);
+        bool deletePreference(Preference *preference);
+        bool deletePreferences(QList<Preference*> preferences);
+
+    signals:
+
+        void newPreferenceCreated();
+        void newPreferencesCreated();
+        void preferenceFound();
+        void preferencePatched();
+        void preferenceSaved();
+        void preferencesSaved();
+        void preferenceDeleted();
+        void preferencesDeleted();
 };
 
 #endif // PREFERENCESTABLE_H
